8196ebcdb808ff173cf2b0fc52f084b2b2fe3dbf
techieshruti <shrutichandra721@gmail.com>
2025-09-25 14:05:42 +0530

    added list in JSX

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const parent=React.createElement("div", {id:"parent"}, 
-    React.createElement("div", {id:"child"},
-        React.createElement("h1", {}, "i am heading")
-    ))
-
 const para = 
 <>
 <h1>Welcome to React tutorial</h1>
 <p className="para">React is Front-end JavaScript library. React is developed by facebook developer. React is tool for building UI component. It is also known as ReactJs.</p>
+
+<h3>Important topics to know before jump into React :</h3>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+</ul>  
+
 </>
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(para)
