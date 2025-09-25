9096fd478d5ab1a2de8af88c74d27e1c762e9115
techieshruti <shrutichandra721@gmail.com>
2025-09-25 12:12:49 +0530

    added jsx

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-/*
-<div class="parent">
-        <div class="child">
-            <h1>i am heading</h1>
-        </div>
-    </div>
-*/
 const parent=React.createElement("div", {id:"parent"}, 
     React.createElement("div", {id:"child"},
         React.createElement("h1", {}, "i am heading")
     ))
 
+const para = <p>Welcome to React learning series and here I am learning JSX</p>
+
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+root.render(para)
