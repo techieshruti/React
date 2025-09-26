ae72cc99ce21a780f820b3e7485065d99e840934
techieshruti <shrutichandra721@gmail.com>
2025-09-26 12:27:23 +0530

    added lnk component

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ const para =
         <li>CSS</li>
         <li>JavaScript</li>
     </ul>
-    <img className="img" src="https://static.vecteezy.com/system/resources/thumbnails/013/313/458/small_2x/html-icon-3d-rendering-illustration-free-vector.jpg"/>
-    <img className="img" src="https://www.shutterstock.com/image-illustration/css-es6-vector-logo-design-260nw-2438405083.jpg"/>
-    <img className="img" src="https://static.vecteezy.com/system/resources/previews/027/127/560/non_2x/javascript-logo-javascript-icon-transparent-free-png.png"/>
+
 </div>
+
+//React Component
+const link = ()=>{<a href="https://react.dev/">React Documentation</a>};
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(para)
