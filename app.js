3e56aa0edaa7ba2c89f537e34438c55da0c5202a
techieshruti <shrutichandra721@gmail.com>
2025-09-27 13:09:05 +0530

    Shruti's cafe structure

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-
-//React Component- we can not use react component before initialization
-
-const Text = () => {
-    return <p>I am a React Component</p>
-}
-console.log(Text);
-const para = 
-<div id="container">
-    <h1>Welcome to React Tutorial</h1>
-    <p className="para">React is javascript library. React is developed by facebook developer. React is tool for building UI component. It is also known as ReactJs.</p>
-    <h3>Important Topics to learn before jumo to React</h3>
-    <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-    </ul>
-    <Text/>
-</div>
+/*
+header
+  -logo
+  -nav bar
+body
+  -search
+  -resturant container
+    -resturants card
+footer
+  -copyright
+  -link
+  -address
+  -contact   
+*/
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(para)
-//root.render(<Text/>)
