bb65cf3db1222da3c50f69bff38e44ca809b19ab
techieshruti <shrutichandra721@gmail.com>
2025-09-27 13:47:49 +0530

    added main container 'AppLayout'

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 /*
 header
   -logo
@@ -15,13 +15,26 @@ footer
   -contact   
 */
 
+//header Component
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=1024x1024&w=is&k=20&c=rizmPsIj6qOq-xUv3-uVbI53K0sWTn65awE321rKZm4="></img>
+      </div>
+      ,
+      <div className="nav-container">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
-    return (
-        <div className="app">
-         //header
-         //body
-         //footer   
-        </div>
-    );
-}
-const root=ReactDOM.createRoot(document.getElementById("root"))
+  return <div className="app">//header //body //footer</div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
