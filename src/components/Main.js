510621d1fe38a0888343ec182125bc0084452774
techieshruti <shrutichandra721@gmail.com>
2025-10-07 20:26:52 +0530

    added filter button to updat resturant list

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,9 @@
 import ResturantCard from "./ResturantCard"
-import resList from "../utills/mockData"
 import { useState } from "react";
 import resList from "../utills/mockData";
 
 const Main = () => {
-const [resList, setresList] = useState(resList);
+const [listOfRes, setlistOfRes] = useState(resList);
   return (
     <div className="main-container">
       <div className="search">
@@ -12,10 +11,10 @@ const [resList, setresList] = useState(resList);
         <i className="fa-solid fa-magnifying-glass search-icon"></i>
       </div>
       <button className="filter-btn" onClick={() => {
-        const filterdList= resList.filter(
-          (res) => res.info.avgRating>4.00
+        const filterdList= listOfRes.filter(
+          (resList) => resList.info.avgRating>4.5
         );
-        setresList(filterdList);
+        setlistOfRes(filterdList);
       }}>
         Top Rated Resturant
       </button>
