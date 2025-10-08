c3834e23f7d8602c321476882faa0a9fc1f51a5c
techieshruti <shrutichandra721@gmail.com>
2025-10-08 13:44:47 +0530

    added restaurants from swiggy api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,45 @@
-import ResturantCard from "./ResturantCard"
+import ResturantCard from "./ResturantCard";
 import { useState, useEffect } from "react";
 import resList from "../utills/mockData";
 
 const Main = () => {
-const [listOfRes, setlistOfRes] = useState(resList);
+  const [listOfRes, setlistOfRes] = useState(resList);
 
-useEffect(() => {
-  fetchData();
-}, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-const fetchData = async ()=>{
-  const data = async fetch("");
-}
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6146243&lng=77.3121575&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json?.data?.cards.find((item) => item?.card?.card.id?.includes("restaurant_grid"))?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
 
-return (
+  return (
     <div className="main-container">
       <div className="search">
         <input type="text" placeholder="Search any resturant..." />
         <i className="fa-solid fa-magnifying-glass search-icon"></i>
       </div>
-      <button className="filter-btn" onClick={() => {
-        const filterdList= listOfRes.filter(
-          (resList) => resList.info.avgRating>4.5
-        );
-        setlistOfRes(filterdList);
-      }}>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const filterdList = listOfRes.filter(
+            (resList) => resList.info.avgRating > 4.5
+          );
+          setlistOfRes(filterdList);
+        }}
+      >
         Top Rated Resturant
       </button>
       <div className="rest-container">
-      {/* passing resObj data and resObj is and object so to pass object in JS we use {}. */}
-        {
-          listOfRes.map(resturant => <ResturantCard key={resturant.info.id} resData={resturant}/>)
-        }
-        </div>
+        {/* passing resObj data and resObj is and object so to pass object in JS we use {}. */}
+        {listOfRes.map((resturant) => (
+          <ResturantCard key={resturant.info.id} resData={resturant} />
+        ))}
+      </div>
     </div>
   );
 };
